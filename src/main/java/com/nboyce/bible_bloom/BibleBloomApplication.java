33ab7126bf17b956e9f95b61fdcbc9c51a60757b
nboyce <boycenobukho@gmail.com>
2025-05-10 03:37:13 +0200

    spring boot initization

---
+++ src/main/java/com/nboyce/bible_bloom/BibleBloomApplication.java
@@ -0,0 +1,13 @@
+package com.nboyce.bible_bloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleBloomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleBloomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nboyce/bible_bloom/BibleBloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nboyce.bible_bloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleBloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
