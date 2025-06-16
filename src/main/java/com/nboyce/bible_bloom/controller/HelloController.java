d5312fb8e5dcb100f8d44896edd3e76add0f1f26
nboyce <boycenobukho@gmail.com>
2025-06-17 01:27:58 +0200

    added a folder for controller classes javafx

---
+++ src/main/java/com/nboyce/bible_bloom/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.nboyce.bible_bloom.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    Label label;
+    public void greet(){
+        label.setText("Hello, World!");
+    }
+}
