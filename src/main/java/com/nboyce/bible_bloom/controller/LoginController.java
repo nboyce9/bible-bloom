11cb071ce912bdd95693a5dfbcb87916922a0592
nboyce <boycenobukho@gmail.com>
2025-06-17 23:21:29 +0200

    Login controller content

--- src/main/java/com/nboyce/bible_bloom/controller/LoginController.java
+++ src/main/java/com/nboyce/bible_bloom/controller/LoginController.java
@@ -1,4 +1,21 @@
 package com.nboyce.bible_bloom.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+    private FXMLLoader loader;
+    private Scene scene;
+    private Stage stage;
+    public void login(ActionEvent event) throws IOException {
+        loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        scene = new Scene(loader.load());
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.show();
+    }
 }
