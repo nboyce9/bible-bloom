585cf3dc652c9808d984b4c25d344ebbaa32eb9e
nboyce <boycenobukho@gmail.com>
2025-06-18 00:31:21 +0200

    path to fxml file

--- src/main/java/com/nboyce/bible_bloom/Homepage.java
+++ src/main/java/com/nboyce/bible_bloom/Homepage.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Homepage extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader loader =  new FXMLLoader(getClass().getResource("home.fxml"));
+        FXMLLoader loader =  new FXMLLoader(getClass().getResource("/com/nboyce/bible_bloom/view/home.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setScene(scene);
         stage.show();
