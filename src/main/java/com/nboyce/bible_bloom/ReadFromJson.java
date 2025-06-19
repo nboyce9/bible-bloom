aae44635fc35052025b140609713d036b8b7dedb
nboyce <boycenobukho@gmail.com>
2025-06-19 23:57:08 +0200

    method to get the list of bible books

--- src/main/java/com/nboyce/bible_bloom/ReadFromJson.java
+++ src/main/java/com/nboyce/bible_bloom/ReadFromJson.java
@@ -1,4 +1,11 @@
 package com.nboyce.bible_bloom;
 
+import java.util.List;
+
 public class ReadFromJson {
+
+    public List<String> getBooks (){
+        String[] books = {};
+        return List.of(books);
+    }
 }
