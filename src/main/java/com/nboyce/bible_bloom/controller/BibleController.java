56e308afd1310ddbbcc8889fd332a8e42d5c5a71
nboyce <boycenobukho@gmail.com>
2025-06-19 23:40:54 +0200

    controller content

--- src/main/java/com/nboyce/bible_bloom/controller/BibleController.java
+++ src/main/java/com/nboyce/bible_bloom/controller/BibleController.java
@@ -1,4 +1,32 @@
 package com.nboyce.bible_bloom.controller;
 
-public class BibleController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BibleController implements Initializable {
+    @FXML
+    Label textLabel;
+    @FXML
+    ChoiceBox<String> bookField, chapterField, verseField;
+    private String[] books ={"Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua"};
+
+    public void searchBibleVerse(){
+//        bookField.getItems().add("Genesis");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        bookField.getItems().addAll(books);
+        bookField.setOnAction(this::getBook);
+    }
+    public void getBook(ActionEvent event){
+        String book = bookField.getValue();
+        System.out.println(book);
+    }
 }
