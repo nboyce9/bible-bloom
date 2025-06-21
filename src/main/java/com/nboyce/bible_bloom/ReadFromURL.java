afa916f8c5c6a5468fe169701fa55d45bab8d1be
nboyce <boycenobukho@gmail.com>
2025-06-21 03:55:24 +0200

    refactored method to be used in the bible controller, return s a bible passage

--- src/main/java/com/nboyce/bible_bloom/ReadFromURL.java
+++ src/main/java/com/nboyce/bible_bloom/ReadFromURL.java
@@ -1,33 +1,21 @@
 package com.nboyce.bible_bloom;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Scanner;
+
 
 public class ReadFromURL {
-    public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter a bible book: ");
-            String version = "en-kjv";
-            String book = scanner.nextLine().toLowerCase();
-            Console console = System.console();
-            ReadFromJson readFromJson = new ReadFromJson();
-            for(BibleBook b: readFromJson.getBooks()){
-            System.out.println(b.getBook());
-            }
 
-            String chapter = console.readLine("Enter a chapter: ");
-            String verse = console.readLine("Enter a verse: ");
-
-            URL url = getUrl(version, book, chapter, verse);
+    public static String passage(String book, String chapter, String verse){
+        try{
+            URL url = getUrl(book, chapter, verse);
             // Open a stream to the URL
             BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
 
@@ -43,12 +31,13 @@ public class ReadFromURL {
 
             if(url.toString().contains("/verses/")) {
                 Book bookWrapper = mapper.readValue(jsonBuilder.toString(), Book.class);
-                System.out.println(book + " " + chapter + ":" + bookWrapper.getVerse() + " - " + bookWrapper.getText());
+                return book + " " + chapter + ":" + bookWrapper.getVerse() + " - " + bookWrapper.getText();
             }else{
                 Data wrapper = mapper.readValue(jsonBuilder.toString(), Data.class);
                 // Loop through the verses
                 for (Book data : wrapper.data) {
-                    System.out.println(data.getBook() + " " + data.getChapter() + ":" + data.getVerse() + " - " + data.getText());}
+                    return data.getBook() + " " + data.getChapter() + ":" + data.getVerse() + " - " + data.getText();
+                }
             }
 
         } catch (IOException e) {
@@ -57,10 +46,12 @@ public class ReadFromURL {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
+        return null;
     }
 
-    private static URL getUrl(String version, String book, String chapter, String verse) throws MalformedURLException, URISyntaxException {
+    public static URL getUrl(String book, String chapter, String verse) throws MalformedURLException, URISyntaxException {
         URL url;
+        String version = "en-kjv";
         String baseURL = "https://cdn.jsdelivr.net/gh/wldeh/bible-api/bibles/" + version + "/books/" + book + "/chapters/" + chapter;
 
         // Create a different URL object for verse or no verse
