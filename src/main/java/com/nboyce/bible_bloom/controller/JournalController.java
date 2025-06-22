47483b2323e0c70959243cd8239b9a6e4997ad34
nboyce <boycenobukho@gmail.com>
2025-06-23 01:40:49 +0200

    clear btn behaviour

--- src/main/java/com/nboyce/bible_bloom/controller/JournalController.java
+++ src/main/java/com/nboyce/bible_bloom/controller/JournalController.java
@@ -1,9 +1,11 @@
 package com.nboyce.bible_bloom.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,16 +14,22 @@ public class JournalController {
     private Stage stage;
     private FXMLLoader loader;
     private Scene scene;
+    @FXML
+    TextArea journalArea;
     public void journalPage() throws IOException {
-        loader = new FXMLLoader(getClass().getResource("bible.fxml"));
+        loader = new FXMLLoader(getClass().getResource("journal.fxml"));
         scene = new Scene(loader.load());
         stage.show();
     }
-    public void journal(ActionEvent event) throws IOException{
-        loader = new FXMLLoader(getClass().getResource("/com/nboyce/bible_bloom/view/journal.fxml"));
-        scene = new Scene(loader.load());
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
+//    public void journal(ActionEvent event) throws IOException{
+//        loader = new FXMLLoader(getClass().getResource("/com/nboyce/bible_bloom/view/journal.fxml"));
+//        scene = new Scene(loader.load());
+//        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+
+    public void clear(ActionEvent event){
+        journalArea.clear();
     }
 }
