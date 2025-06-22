194607f84a8a0c861515acbff5a68f57b1c204a7
nboyce <boycenobukho@gmail.com>
2025-06-23 01:15:34 +0200

    controller for journaling feature

---
+++ src/main/java/com/nboyce/bible_bloom/controller/JournalController.java
@@ -0,0 +1,27 @@
+package com.nboyce.bible_bloom.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class JournalController {
+    private Stage stage;
+    private FXMLLoader loader;
+    private Scene scene;
+    public void journalPage() throws IOException {
+        loader = new FXMLLoader(getClass().getResource("journal.fxml"));
+        scene = new Scene(loader.load());
+        stage.show();
+    }
+    public void journal(ActionEvent event) throws IOException{
+        loader = new FXMLLoader(getClass().getResource("/com/nboyce/bible_bloom/view/bible.fxml"));
+        scene = new Scene(loader.load());
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+}
