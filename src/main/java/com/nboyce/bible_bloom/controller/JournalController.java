6680fba9e7ed6e6ba6e81e3899956839212cd1ed
nboyce <boycenobukho@gmail.com>
2025-06-23 01:36:01 +0200

    skeleton

--- src/main/java/com/nboyce/bible_bloom/controller/JournalController.java
+++ src/main/java/com/nboyce/bible_bloom/controller/JournalController.java
@@ -13,12 +13,12 @@ public class JournalController {
     private FXMLLoader loader;
     private Scene scene;
     public void journalPage() throws IOException {
-        loader = new FXMLLoader(getClass().getResource("journal.fxml"));
+        loader = new FXMLLoader(getClass().getResource("bible.fxml"));
         scene = new Scene(loader.load());
         stage.show();
     }
     public void journal(ActionEvent event) throws IOException{
-        loader = new FXMLLoader(getClass().getResource("/com/nboyce/bible_bloom/view/bible.fxml"));
+        loader = new FXMLLoader(getClass().getResource("/com/nboyce/bible_bloom/view/journal.fxml"));
         scene = new Scene(loader.load());
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.setScene(scene);
