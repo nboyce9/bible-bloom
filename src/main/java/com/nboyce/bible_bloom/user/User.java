c23f9e7bf66a0b2f8e1590b062094bb4ea8d1139
nboyce <boycenobukho@gmail.com>
2025-06-26 23:10:04 +0200

    fields

--- src/main/java/com/nboyce/bible_bloom/user/User.java
+++ src/main/java/com/nboyce/bible_bloom/user/User.java
@@ -1,12 +1,16 @@
 package com.nboyce.bible_bloom.user;
 
 public class User {
-
+    String username;
+    String email;
+    String password;
 
     public User(){
 
     }
     public User(String username, String email, String password){
-
+        this.username = username;
+        this.email = email;
+        this.password = password;
     }
 }
