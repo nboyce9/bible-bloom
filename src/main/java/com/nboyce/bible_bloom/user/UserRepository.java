1d77265fc235ce37828b2efc61458ee8639d727a
nboyce <boycenobukho@gmail.com>
2025-06-26 23:40:52 +0200

    repository to store user details

---
+++ src/main/java/com/nboyce/bible_bloom/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.nboyce.bible_bloom.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<Integer, User> {
+
+}
